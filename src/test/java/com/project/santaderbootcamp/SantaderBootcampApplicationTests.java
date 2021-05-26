8f556275b3f5ac6e5041356f87f35e558b5cf2c6
Edson Alves <edsonapf@gmail.com>
2021-05-26 19:31:13 -0300

    initial commit

---
+++ src/test/java/com/project/santaderbootcamp/SantaderBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.santaderbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaderBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
